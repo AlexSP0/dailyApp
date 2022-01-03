0fea8060b5f97feaf0b4587a23c55a69d1ea51b4
Alex <workernight@yandex.ru>
2022-01-03 10:02:02 +0400

    Add wiki search and adaptive icon search

--- app/src/main/java/com/alexsp0/dailyapp/ui/MainFragment.kt
+++ app/src/main/java/com/alexsp0/dailyapp/ui/MainFragment.kt
@@ -1,5 +1,7 @@
 package com.alexsp0.dailyapp.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,6 +15,8 @@ import com.alexsp0.dailyapp.data.NasaImageResponse
 import com.alexsp0.dailyapp.presenters.MainPresenterImpl
 import com.bumptech.glide.Glide
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainFragment(presenter:MainPresenterImpl) : Fragment() {
     private var presenter : MainPresenterImpl = presenter
@@ -20,6 +24,8 @@ class MainFragment(presenter:MainPresenterImpl) : Fragment() {
     private lateinit var  bottomSheetBehavior : BottomSheetBehavior<ConstraintLayout>
     private lateinit var bottomSheetHeader : TextView
     private lateinit var bottomSheetDescription : TextView
+    private lateinit var input_wiki_text_layout : TextInputLayout
+    private lateinit var input_text_search_wiki : TextInputEditText
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         presenter.attach(this)
@@ -32,8 +38,21 @@ class MainFragment(presenter:MainPresenterImpl) : Fragment() {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
         image = view.findViewById(R.id.image)
         setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
+        initSearchWikiIcon(view)
         presenter.getDailyImage()
         return view
+    }
+
+    private fun initSearchWikiIcon(view : View) {
+        input_wiki_text_layout = view.findViewById<TextInputLayout>(R.id.input_text_search_wiki_layout)
+        input_text_search_wiki = view.findViewById<TextInputEditText>(R.id.input_edit_text_search_wiki)
+        input_wiki_text_layout.setEndIconOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            val url = "https://en.wikipedia.org/wiki/${input_text_search_wiki.text.toString()}"
+            val uri = Uri.parse(url)
+            intent.data = uri
+            startActivity(intent)
+        }
     }
 
     fun setBottomSheetBehavior(bottomSheet : ConstraintLayout) {
@@ -49,6 +68,11 @@ class MainFragment(presenter:MainPresenterImpl) : Fragment() {
         bottomSheetHeader.text = "Header"
         bottomSheetDescription.text = "Description"
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.detach()
     }
 
     companion object {
